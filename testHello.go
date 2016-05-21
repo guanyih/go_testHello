a4903f19747c5725319246a81b084e03e4ec62b1
Guanyi Hao <guanyih@msn.com>
2016-05-21 21:58:16 +0800

    initial commit

---
+++ testHello.go
@@ -0,0 +1,17 @@
+// testhello
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	who := "World!"
+	if len(os.Args) > 1 {
+		who = strings.Join(os.Args[1:], " ")
+
+	}
+	fmt.Println("Hello, Guanyi and ", who)
+}
